1a537b90b909d7bf831fafd0b035ccb41997eeb7
BruxoSagaz <umpoucodetudogr@gmail.com>
2022-05-10 16:22:12 -0300

    init login gerency

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
-
+session_start();
 define("PATH","https://localhost/Lojinha/");
 
 $autoload = function($class){
     if($class == 'Email'){
         include('classes/phpmailer/src/PHPMailer.php');
     }
-    include('classes/'.$class.'php');
+    include('classes/'.$class.'.php');
 };
 
 spl_autoload_register($autoload);
---
+++ gerency/login.php
@@ -0,0 +1,5 @@
+<?php
+
+
+
+?>pag de login
